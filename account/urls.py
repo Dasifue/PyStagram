8c16461bf24173639ab76f63632760b24a16c00f
dasifue <dasifue@gmail.com>
2023-09-01 22:09:40 +0600

    registration and login

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from .views import (
+    register_view,
+    login_view,
+)
+
 app_name = "account"
 
-urlpatterns = [
 
+urlpatterns = [
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from .forms import (
+    RegisterForm,
+    LoginForm
+)
+
+
+def register_view(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("account:login")
+        else:
+            return render(request, "sign-up.html", {"form": form})
+    else:   
+        return render(request, "sign-up.html", {"form": form})
+    
+
+def login_view(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        user = authenticate(username=form.data["username"], password=form.data["password"])
+        if user is not None:
+            login(request, user)
+            return redirect("blog:posts_list")
+        else:
+            return render(request, "sign-in.html", {"form": form, "error": "Неверное имя пользователя или пароль!"})
+    else:
+        return render(request, "sign-in.html", {"form": form})
