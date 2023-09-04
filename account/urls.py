c4883bb327ecefcbc0258f6406e76b52cb12de4b
dasifue <dasifue@gmail.com>
2023-09-04 21:37:17 +0600

    user profile and details

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import (
     register_view,
     login_view,
+    logout_view,
+    profile_view,
 )
 
 app_name = "account"
@@ -11,4 +13,6 @@ app_name = "account"
 urlpatterns = [
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("profile/<int:pk>", profile_view, name="profile")
 ]
